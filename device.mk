fe628c7be2e8e11f5b8a5ad58c7799ca588c2791
Michael Bestas <mkbestas@lineageos.org>
2022-11-09 21:57:09 +0530

    r5x: Remove android.hardware.graphics.composer@2.4-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- device.mk
+++ device.mk
@@ -129,7 +129,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.frameworks.displayservice@1.0 \
-    android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
